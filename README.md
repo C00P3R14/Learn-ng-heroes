9d8539068965535c0e1a7b714b872121ee3fb712
C00P3R14 <96924801+C00P3R14@users.noreply.github.com>
2024-02-08 14:01:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learn-ng-heroes
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/C00P3R14/Learn-ng-heroes)
---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core'
+import { HeroesComponent } from './heroes/heroes.component'
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [HeroesComponent],
+})
+export class AppComponent {
+  title = 'Tour of Heroes'
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
