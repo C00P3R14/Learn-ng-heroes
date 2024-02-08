c69b219a997f3f56190b0a1f140e3bba39ecf43c
C00P3R14 <ngubane.linda9@gmail.com>
2024-02-08 15:22:38 +0200

    Import AppRoutingModule in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core'
 import { HeroesComponent } from './heroes/heroes.component'
 import { MessagesComponent } from './messages/messages.component'
+import { AppRoutingModule } from './app-routing.module'
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [HeroesComponent, MessagesComponent],
+  imports: [HeroesComponent, MessagesComponent, AppRoutingModule],
 })
 export class AppComponent {
   title = 'Tour of Heroes'
